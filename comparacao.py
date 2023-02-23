2036282d4e601190614ddb4a9478332b925251f0
JulianoOrlandi <juliano.orlandi1982@gmail.com>
2023-02-23 07:57:36 -0300

    Second Commit

--- comparacao.py
+++ comparacao.py
@@ -3,7 +3,8 @@ import date_calculator
 
 
 def main():
-    
+    print("Control")    
+    """
     months_31 = [1, 3, 5, 7, 8, 10, 12]
     months_30 = [4, 6, 9, 11]
     
@@ -22,7 +23,7 @@ def main():
                 else:
                     for k in range(1, 29):
                         test_calc(i, j, k)
-
+    """
 def test_calc(year, month, day):
     my_initial = date_calculator.Date(1, 1, 1)
     datetime_initial = datetime.date(1, 1, 1)
